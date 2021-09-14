638a48aad34afef48037629bdf44b2c11ab7ed64
EceAtes <71889445+EceAtes@users.noreply.github.com>
2021-09-14 12:34:51 +0300

    Update DepthFirstSearch.java

--- DepthFirstSearch/src/DepthFirstSearch.java
+++ DepthFirstSearch/src/DepthFirstSearch.java
@@ -38,10 +38,10 @@ public class DepthFirstSearch {
 	MIA.connect(PBI);
 	
 	
-//		DFW---JFK---MIA---MCO
-//		 |     |     \	  /
-//		LAX   BOS      PBI
-//		/|\
+//	    DFW---JFK---MIA---MCO
+//	     |     |      \    /
+//	    LAX   BOS       PBI
+//	    /|\
 //	   / | \
 //	HNL SAN EWR
 	
